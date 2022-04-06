a2ca557189d53b6ba56bc000a9e1a4edbc4287ac
celjr <cjuniordev@gmail.com>
2022-04-06 01:34:20 -0300

    alterações das aulas 5 e 6, qaprendendo query parameters e o post

--- bin/server_handler.dart
+++ bin/server_handler.dart
@@ -1,4 +1,4 @@
-import 'dart:ffi';
+import 'dart:convert';
 
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
@@ -8,6 +8,30 @@ class ServeHandler {
     final router = Router();
     router.get('/', (Request request) {
       return Response(200, body: 'Primeira rota');
+    });
+
+    router.get('/ola/mundo/<usuario>', (Request req, String usuario) {
+      return Response.ok("Ola Mundo $usuario");
+    });
+    // http://localhost:8080/query?nome=Junior
+    router.get('/query', (Request req) {
+      String? nome = req.url.queryParameters['nome'];
+      String? idade = req.url.queryParameters['idade'];
+      return Response.ok('Query eh: $nome,  $idade');
+    });
+
+    router.post('/login', (Request req) async {
+      var result = await req.readAsString();
+      Map json = jsonDecode(result);
+
+      var usuario = json['usuario'];
+      var senha = json['usuario'];
+
+      if (usuario == 'admin' && senha == '123') {
+        return Response.ok('Bem vindo $usuario');
+      } else {
+        return Response.ok('acesso negado');
+      }
     });
 
     return router;
